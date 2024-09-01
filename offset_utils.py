795fe7033efd2ee94e8ba007129e376ea34b0389
khs8157 <55694871+khs8157@users.noreply.github.com>
2024-09-01 15:03:47 +0900

    Update offset_utils.py

--- offset_utils.py
+++ offset_utils.py
@@ -126,7 +126,7 @@ def get_magnitude(fourier):
     magnitude4 = np.zeros_like(magnitude)
     magnitude4[:-(-m//4)] = magnitude[::4]
     
-    return magnitude + magnitude2 + magnitude3 + magnitude4
+    return magnitude*1.5 + magnitude2*0.75 + magnitude3*0.75 + magnitude4*0.75
 
 
 def offset_from_fourier(fourier, idx, fps=50, num_windows=21, bpm_scale=0.1):
